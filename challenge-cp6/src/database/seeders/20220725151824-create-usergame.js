68051d6d5f7cecdfb24c96db6dd76bdf833550b1
mbaihaqi23 <mbaihaqi23@gmail.com>
2022-07-25 22:24:10 +0700

    challenge chapter 6

---
+++ challenge-cp6/src/database/seeders/20220725151824-create-usergame.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert("user_games", [
+
+      {
+      
+      username: "superuser",
+      
+      password: "iamsuperuser",
+            
+      createdAt: new Date(),
+      
+      updatedAt: new Date(),
+      
+      },
+      
+      ]);
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
